75c3c4c46b2947fe7ffcad07117b4b56544eca01
Hoon Hwangbo <drawtree@gmail.com>
2011-12-25 13:52:08 +0900

    Done.

---
+++ Script/Helper/Helper/Help/AAAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AAAppDelegate.h
+//  Help
+//
+//  Created by Hoon Hwangbo on 12/24/11.
+//  Copyright (c) 2011 Eonil Company. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AAAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Script/Helper/Helper/Help/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Help
+//
+//  Created by Hoon Hwangbo on 12/24/11.
+//  Copyright (c) 2011 Eonil Company. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AAAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AAAppDelegate class]));
+	}
+}
